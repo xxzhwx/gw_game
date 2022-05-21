011b243e0d6438a1e8efaab7ef0cd0522eeffa6d
mklin <mklin_hi@163.com>
2022-05-21 15:03:16 +0800

    logger

---
+++ snextra/service/logger.lua
@@ -0,0 +1,64 @@
+local skynet = require "skynet"
+local logger = {}
+
+local LOG_LEVEL = {
+    debug = 1,
+    info = 2,
+    warn = 3,
+    error = 4,
+}
+
+local function init_log_level()
+    if not logger.level then
+        local level = skynet.getenv("log_level")
+        if not level or not LOG_LEVEL[level] then
+            level = LOG_LEVEL.debug
+        end
+
+        logger.level = level
+    end
+end
+
+function logger.set_log_level(name)
+    local level = LOG_LEVEL[name]
+    if level then
+        logger.level = level
+    end
+end
+
+local function log(level, format, ...)
+    local str = string.format(format, ...)
+    skynet.send(".loggerd", "lua", "log", level, str)
+end
+
+function logger.debug(format, ...)
+    if logger.level >= LOG_LEVEL.debug then
+        log(LOG_LEVEL.debug, format, ...)
+    end
+end
+
+function logger.info(format, ...)
+    if logger.level >= LOG_LEVEL.info then
+        log(LOG_LEVEL.info, format, ...)
+    end
+end
+
+function logger.warn(format, ...)
+    if logger.level >= LOG_LEVEL.warn then
+        log(LOG_LEVEL.warn, format, ...)
+    end
+end
+
+function logger.error(format, ...)
+    if logger.level >= LOG_LEVEL.error then
+        log(LOG_LEVEL.error, format, ...)
+    end
+end
+
+init_log_level()
+
+skynet.init(function ()
+    skynet.uniqueservice("loggerd")
+end)
+
+return logger
---
+++ snextra/service/loggerd.lua
@@ -0,0 +1,101 @@
+local skynet = require "skynet"
+require "skynet.manager"
+
+local log_dir = skynet.getenv("log_dir") or "./log"
+local log_filename = skynet.getenv("log_filename") or "default"
+
+local is_shutdown = false
+local queue = {}
+local log_file = {}
+
+local LEVEL_DESC = {
+    [1] = "[debug]",
+    [2] = "[info]",
+    [3] = "[warn]",
+    [4] = "[error]",
+}
+
+local CMD = {}
+
+function CMD.log(level, str)
+    local timestamp = math.floor(skynet.time())
+    local date_desc = os.date("%Y%m%d", timestamp)
+    local time_desc = os.date("[%Y-%m-%d %H:%M:%S]", timestamp)
+
+    table.insert(queue, {
+        level = LEVEL_DESC[level],
+        str = str,
+        date_desc = date_desc,
+        time_desc = time_desc,
+    })
+end
+
+local function close_log_file()
+    if log_file[2] then
+        log_file[2]:close()
+        log_file[2] = nil
+
+        log_file[1] = nil
+    end
+end
+
+local function run_once()
+    local ok, err = xpcall(function ()
+        for _, info in ipairs(queue) do
+            local filepath = string.format("%s_%s.log", log_filename, info.date_desc)
+            if filepath ~= log_file[1] then
+                close_log_file()
+
+                local f = io.open(filepath)
+                if f then
+                    log_file[1] = filepath
+                    log_file[2] = f
+                end
+            end
+
+            local f = log_file[2]
+            if f then
+                f:write(string.format("%s%s %s\n", info.level, info.time_desc, info.str))
+            end
+        end
+    end, debug.traceback)
+
+    if not ok then
+        skynet.error(err)
+    end
+
+    if is_shutdown then
+        close_log_file()
+    end
+end
+
+local function run()
+    while true do
+        skynet.sleep(300)
+        if not is_shutdown then
+            run_once()
+        end
+    end
+end
+
+function CMD.shutdown()
+    if is_shutdown then return end
+    is_shutdown = true
+    run_once()
+end
+
+skynet.start(function ()
+    os.execute("mkdir -p " .. log_dir)
+
+    skynet.register(".loggerd")
+    skynet.dispatch("lua", function (session, source, cmd, ...)
+        local f = assert(CMD[cmd], "unknown command: " .. cmd)
+        if session > 0 then
+            skynet.retpack(f(...))
+        else
+            f(...)
+        end
+    end)
+
+    skynet.timeout(0, run)
+end)
